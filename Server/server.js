757476a630bf969d25eeb03d388cf9efea140788
Aniketbajaj2024 <bajajaniket2024@gmail.com>
2025-06-17 17:15:15 +0530

    project_init

---
+++ Server/server.js
@@ -0,0 +1,26 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const cors = require("cors");
+require("dotenv").config();
+
+const app = express();
+const PORT = process.env.PORT || 5000;
+
+app.use(cors());
+app.use(express.json());
+
+app.get("/", (req, res) => {
+  res.send("Job Tracker API is running 🚀");
+});
+
+mongoose
+  .connect(process.env.MONGO_URI, {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    app.listen(PORT, () =>
+      console.log(`Server started on http://localhost:${PORT}`)
+    );
+  })
+  .catch((err) => console.log("DB Connection Error:", err.message));
