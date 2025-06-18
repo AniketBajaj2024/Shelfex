a15b566ebb85dccd714cbd5a5b11dea4df02dfd2
Aniketbajaj2024 <bajajaniket2024@gmail.com>
2025-06-19 02:27:14 +0530

    loginFunctionaltyinsignup

--- client/src/Pages/Signup.js
+++ client/src/Pages/Signup.js
@@ -69,6 +69,10 @@ export default function Signup() {
 
         <button type="submit">Sign Up</button>
       </form>
+      <p style={{ marginTop: "1rem" }}>
+  Don't have an account? <a href="/signup">Sign up here</a>
+</p>
+
     </div>
   );
 }
